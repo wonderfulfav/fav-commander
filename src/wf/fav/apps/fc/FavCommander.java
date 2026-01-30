3432cbf5cf986aeb8b6dd5b10e74ab84254e852b
Wonderful FAV <slady-worldoffav@slady.net>
2026-01-30 15:52:17 +0100

    Update FavCommander.java
    add gui window

--- src/wf/fav/apps/fc/FavCommander.java
+++ src/wf/fav/apps/fc/FavCommander.java
@@ -2,6 +2,22 @@ package wf.fav.apps.fc;
 
 public class FavCommander {
     static void main() {
+        final JFrame f = new JFrame("fav commander");
+        f.setVisible(true);
+        f.setLocation(100, 100);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        final FavCommanderMainWindow m = new FavCommanderMainWindow();
+        f.getContentPane().add(m);
+        m.grabFocus();
+        f.pack();
+        m.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(final KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+                    m.shutDown();
+                }
+            }
+        });
         System.out.println("hello world");
     }
 }
