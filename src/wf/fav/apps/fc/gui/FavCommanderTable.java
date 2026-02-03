8f2939d5d2bf788e3fe24d359625ba179e709248
wonderfulfav <slady-worldoffav@slady.net>
2026-02-04 00:02:28 +0100

    FavCommanderTable displays files, moves cursor and Insert key selects files

--- src/wf/fav/apps/fc/gui/FavCommanderTable.java
+++ src/wf/fav/apps/fc/gui/FavCommanderTable.java
@@ -6,7 +6,9 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class FavCommanderTable extends JComponent {
 
@@ -16,7 +18,33 @@ public class FavCommanderTable extends JComponent {
     private static final Color CURSOR_FOREGROUND = BACKGROUND;
     private static final Color HIGHLIGHTED = Color.GREEN;
 
+    private static final long MAGNITUDE = 1024;
+    private static final long KB = MAGNITUDE;
+    private static final long MB = KB * MAGNITUDE;
+    private static final long GB = MB * MAGNITUDE;
+    private static final long TB = GB * MAGNITUDE;
+    private static final long PB = TB * MAGNITUDE;
+
+    private static final long KB10 = 10 * KB;
+    private static final long MB10 = 10 * MB;
+    private static final long GB10 = 10 * GB;
+    private static final long TB10 = 10 * TB;
+
+    private static final String SPACE = " ";
+    private static final String SPACE2 = SPACE + SPACE;
+    private static final String SPACE3 = SPACE2 + SPACE;
+
+    // or make it KiB, MiB, GiB, TiB, PiB?
+    private static final String B = SPACE + "B";
+    private static final String KIB = SPACE + "K";
+    private static final String MIB = SPACE + "M";
+    private static final String GIB = SPACE + "G";
+    private static final String TIB = SPACE + "T";
+    private static final String PIB = SPACE + "P";
+
     private List<? extends FavCommanderFile> fileList;
+    private final Set<FavCommanderFile> selectedSet = new HashSet<>();
+    private int cursorIndex;
 
     public FavCommanderTable() {
         addKeyListener(new KeyAdapter() {
@@ -30,19 +58,51 @@ public class FavCommanderTable extends JComponent {
     public void keyPressedAction(final int keyCode) {
         switch (keyCode) {
             case KeyEvent.VK_ESCAPE -> System.exit(0);
+
+            case KeyEvent.VK_UP -> {
+                if (cursorIndex > 0) {
+                    cursorIndex--;
+                    repaint();
+                }
+            }
+
+            case KeyEvent.VK_DOWN -> {
+                if (fileList != null && cursorIndex < fileList.size() - 1) {
+                    cursorIndex++;
+                    repaint();
+                }
+            }
+
+            case KeyEvent.VK_INSERT -> {
+                final FavCommanderFile selectedFile = fileList.get(cursorIndex);
+
+                if (selectedSet.contains(selectedFile)) {
+                    selectedSet.remove(selectedFile);
+                } else {
+                    selectedSet.add(selectedFile);
+                }
+
+                if (fileList != null && cursorIndex < fileList.size() - 1) {
+                    cursorIndex++;
+                }
+
+                repaint();
+            }
+
             default -> System.out.println(keyCode);
         }
     }
 
     @Override
     public Dimension getPreferredSize() {
-        return new Dimension(550, 350);
+        return new Dimension(550, 380);
     }
 
     @Override
-    public void paint(Graphics g) {
+    public void paint(final Graphics g) {
         int width = getWidth();
         int height = getHeight();
+
         g.setColor(BACKGROUND);
         g.fillRect(0, 0, width, height);
 
@@ -50,27 +110,78 @@ public class FavCommanderTable extends JComponent {
             return;
         }
 
-        g.setColor(FOREGROUND);
+        g.setColor(CURSOR_BACKGROUND);
+        g.fillRect(0, cursorIndex * 20, width, 20);
 
         for (int i = 0; i < fileList.size(); i++) {
-            FavCommanderFile f = fileList.get(i);
-            g.drawString(f.getName(), 10, i * 20);
-
-            if (f.isDirectory()) {
-                g.drawRect(5, i * 20, 3, 3);
-            } else {
-                g.drawString(Long.toString(f.getFileSize()), 250, i * 20);
-            }
+            g.setColor((i == cursorIndex) ? CURSOR_FOREGROUND : FOREGROUND);
+            drawFile(g, i);
         }
     }
 
+    public void drawFile(final Graphics g, final int i) {
+        final FavCommanderFile f = fileList.get(i);
+
+        if (f.isDirectory()) {
+            g.drawString("<DIR>", 250, i * 20 + 12);
+        } else {
+            g.drawString(humanReadableSize(f.getFileSize()), 250, i * 20 + 12);
+        }
+
+        if (selectedSet.contains(f)) {
+            g.setColor(HIGHLIGHTED);
+        }
+
+        g.drawString(f.getName(), 10, i * 20 + 12);
+    }
+
+    public static String humanReadableSize(final long size) {
+        if (size > PB) {
+            return leftPad(size / PB) + PIB;
+        }
+        if (size > TB10) {
+            return leftPad(size / TB) + TIB;
+        }
+
+        if (size > GB10) {
+            return leftPad(size / GB) + GIB;
+        }
+
+        if (size > MB10) {
+            return leftPad(size / MB) + MIB;
+        }
+
+        if (size > KB10) {
+            return leftPad(size / KB) + KIB;
+        }
+
+        return leftPad(size) + B;
+    }
+
+    public static String leftPad(final long size) {
+        if (size < 10) {
+            return SPACE3 + size;
+        }
+
+        if (size < 100) {
+            return SPACE2 + size;
+        }
+
+        if (size < 1000) {
+            return SPACE + size;
+        }
+
+        return Long.toString(size);
+    }
+
     @Override
-    public void update(Graphics g) {
+    public void update(final Graphics g) {
         paint(g);
     }
 
-    public void setFileList(List<? extends FavCommanderFile> fileList) {
+    public void setFileList(final List<? extends FavCommanderFile> fileList) {
         this.fileList = fileList;
+        selectedSet.clear();
         repaint();
     }
 
