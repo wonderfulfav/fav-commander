ff338a1ce3bbfba3427ba602f8d80124eb23481e
wonderfulfav <slady-worldoffav@slady.net>
2026-02-03 09:54:14 +0100

    FavCommanderFileComparator refactoring

--- src/wf/fav/apps/fc/sort/FavCommanderFileComparator.java
+++ src/wf/fav/apps/fc/sort/FavCommanderFileComparator.java
@@ -5,7 +5,13 @@ import wf.fav.apps.fc.fs.FavCommanderFile;
 import java.util.Comparator;
 
 public class FavCommanderFileComparator {
-    private static final Comparator<FavCommanderFile> DIRECTORY_LOCAL = new FavCommanderIsDirectoryComparator();
-    private static final Comparator<FavCommanderFile> NAME_LOCAL = new FavCommanderFileNameComparator();
-    public static Comparator<FavCommanderFile> NAME = DIRECTORY_LOCAL.thenComparing(NAME_LOCAL);
+
+    private static final Comparator<FavCommanderFile> DIRECTORY_LOCAL =
+            (a, b) -> -Boolean.compare(a.isDirectory(), b.isDirectory());
+
+    private static final Comparator<FavCommanderFile> NAME_LOCAL =
+            (a, b) -> a.getName().compareToIgnoreCase(b.getName());
+
+    public static final Comparator<FavCommanderFile> NAME = DIRECTORY_LOCAL.thenComparing(NAME_LOCAL);
+
 }
