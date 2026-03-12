8e426c4afb37c99d54d0006ed4d69d9e2d66970e
wonderfulfav <slady-worldoffav@slady.net>
2026-03-12 15:40:19 +0100

    position FavCommanderTableView using BorderLayout.LINE_START

--- src/wf/fav/apps/fc/gui/FavCommanderMainWindow.java
+++ src/wf/fav/apps/fc/gui/FavCommanderMainWindow.java
@@ -4,6 +4,7 @@ import wf.fav.apps.fc.fs.local.LocalFavCommanderFileSystem;
 import wf.fav.apps.fc.gui.table.FavCommanderTableView;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class FavCommanderMainWindow extends JFrame {
 
@@ -12,8 +13,10 @@ public class FavCommanderMainWindow extends JFrame {
         setVisible(true);
         setLocation(100, 100);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        final Container p = getContentPane();
         final FavCommanderTableView m = new FavCommanderTableView();
-        getContentPane().add(m);
+        // BorderLayout is the default layout
+        p.add(m, BorderLayout.LINE_START);
         m.grabFocus();
         pack();
 
