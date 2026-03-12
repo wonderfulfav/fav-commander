04a9b2b977109d0b425acbaeff8d3da01a1b9f4e
wonderfulfav <slady-worldoffav@slady.net>
2026-03-12 15:31:03 +0100

    refactoring: moving window creation to the FavCommanderMainWindow class

--- src/wf/fav/apps/fc/FavCommander.java
+++ src/wf/fav/apps/fc/FavCommander.java
@@ -1,27 +1,13 @@
 package wf.fav.apps.fc;
 
-import wf.fav.apps.fc.fs.local.LocalFavCommanderFileSystem;
-import wf.fav.apps.fc.gui.table.FavCommanderTableView;
+import wf.fav.apps.fc.gui.FavCommanderMainWindow;
 
-import javax.swing.*;
 import java.io.File;
 
 public class FavCommander {
     static void main() {
-        final JFrame f = new JFrame("fav commander");
-        f.setVisible(true);
-        f.setLocation(100, 100);
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         new File(System.getProperty("user.dir"));
-//        final FavCommanderMainWindow m = new FavCommanderMainWindow();
-        final FavCommanderTableView m = new FavCommanderTableView();
-        f.getContentPane().add(m);
-        m.grabFocus();
-        f.pack();
-
-        m.getModel().setCurrentDirectory(
-                LocalFavCommanderFileSystem.getLocalFavCommanderFileSystemInstance()
-                        .listRoots().getFirst());
+        new FavCommanderMainWindow();
     }
 
 }
--- src/wf/fav/apps/fc/gui/FavCommanderMainWindow.java
+++ src/wf/fav/apps/fc/gui/FavCommanderMainWindow.java
@@ -1,6 +1,25 @@
 package wf.fav.apps.fc.gui;
 
+import wf.fav.apps.fc.fs.local.LocalFavCommanderFileSystem;
+import wf.fav.apps.fc.gui.table.FavCommanderTableView;
+
 import javax.swing.*;
 
-public class FavCommanderMainWindow extends JComponent {
+public class FavCommanderMainWindow extends JFrame {
+
+    public FavCommanderMainWindow() {
+        super("fav commander");
+        setVisible(true);
+        setLocation(100, 100);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        final FavCommanderTableView m = new FavCommanderTableView();
+        getContentPane().add(m);
+        m.grabFocus();
+        pack();
+
+        m.getModel().setCurrentDirectory(
+                LocalFavCommanderFileSystem.getLocalFavCommanderFileSystemInstance()
+                        .listRoots().getFirst());
+    }
+
 }
