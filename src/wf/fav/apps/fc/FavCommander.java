52da762dd0946c6c835b145e435ff684c5e9556d
wonderfulfav <slady-worldoffav@slady.net>
2026-03-13 09:53:52 +0100

    upgrade: on startup, the left panel still shows the root directory, but the right panel now shows the user directory

--- src/wf/fav/apps/fc/FavCommander.java
+++ src/wf/fav/apps/fc/FavCommander.java
@@ -1,19 +1,22 @@
 package wf.fav.apps.fc;
 
 import wf.fav.apps.fc.fs.FavCommanderFile;
+import wf.fav.apps.fc.fs.local.LocalFavCommanderFile;
 import wf.fav.apps.fc.fs.local.LocalFavCommanderFileSystem;
 import wf.fav.apps.fc.gui.FavCommanderMainWindow;
 
 import java.io.File;
 
 public class FavCommander {
+
     static void main() {
-        new File(System.getProperty("user.dir"));
+        LocalFavCommanderFile userDir = new LocalFavCommanderFile(
+                new File(System.getProperty("user.dir")));
         final FavCommanderMainWindow m = new FavCommanderMainWindow();
         final FavCommanderFile root = LocalFavCommanderFileSystem
                 .getLocalFavCommanderFileSystemInstance().listRoots().getFirst();
         m.getLeftPanelModel().setCurrentDirectory(root);
-        m.getRightPanelModel().setCurrentDirectory(root);
+        m.getRightPanelModel().setCurrentDirectory(userDir);
     }
 
 }
