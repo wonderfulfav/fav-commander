6c069d1506c0ce67b00b56eabe195a207b3ba87e
wonderfulfav <slady-worldoffav@slady.net>
2026-04-01 15:17:53 +0200

    upgrade: two dots as a parent directory sorted as the first file in the list

--- src/wf/fav/apps/fc/sort/FavCommanderFileComparator.java
+++ src/wf/fav/apps/fc/sort/FavCommanderFileComparator.java
@@ -1,10 +1,24 @@
 package wf.fav.apps.fc.sort;
 
 import wf.fav.apps.fc.fs.FavCommanderFile;
+import wf.fav.apps.fc.fs.ParentFavCommanderFile;
 
 import java.util.Comparator;
 
 public class FavCommanderFileComparator {
+
+    public static final Comparator<FavCommanderFile> PARENT_DIRECTORY_LOCAL
+            = (file1, file2) -> {
+        if (file1 instanceof ParentFavCommanderFile) {
+            return -1;
+        }
+
+        if (file2 instanceof ParentFavCommanderFile) {
+            return 1;
+        }
+
+        return 0;
+    };
 
     private static final Comparator<FavCommanderFile> DIRECTORY_LOCAL =
             (a, b) -> -Boolean.compare(a.isDirectory(), b.isDirectory());
@@ -12,6 +26,9 @@ public class FavCommanderFileComparator {
     private static final Comparator<FavCommanderFile> NAME_LOCAL =
             (a, b) -> a.getName().compareToIgnoreCase(b.getName());
 
-    public static final Comparator<FavCommanderFile> NAME = DIRECTORY_LOCAL.thenComparing(NAME_LOCAL);
+    public static final Comparator<FavCommanderFile> NAME =
+            PARENT_DIRECTORY_LOCAL.thenComparing(
+                    DIRECTORY_LOCAL.thenComparing(
+                            NAME_LOCAL));
 
 }
